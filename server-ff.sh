e37e0264f9377b92c517af4bb2c2ba9bde761ad3
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-12 00:29:09 +0800

    Create server-ff.sh

---
+++ server-ff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
+source /opt/venv/bin/activate
+# nohup python /data/app.py > /data/app.log 2>&1 &
+nohup python /data/app.py > /dev/null 2>&1 &
+./ff.sh
+# jupyter lab \
+#     --ip=0.0.0.0 \
+#     --port=7860 \
+#     --no-browser \
+#     --allow-root \
+#     --notebook-dir=/data \
+#     --ServerApp.token="$JUPYTER_TOKEN" \
+#     --ServerApp.disable_check_xsrf=True
