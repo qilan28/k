25b6b81fe291bc579329cf6b99ac53f00a208d66
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-12 13:29:26 +0800

    Update server-ff.sh

--- server-ff.sh
+++ server-ff.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
-wget -O "ff.py" "https://raw.githubusercontent.com/qilan28/k/refs/heads/main/ff.py"
+# wget -O "ff.py" "https://raw.githubusercontent.com/qilan28/k/refs/heads/main/ff.py"
 # source /opt/venv/bin/activate
 # nohup python /data/app.py > /data/app.log 2>&1 &
 # nohup python /data/app.py > /dev/null 2>&1 &
+ls
 python ff.py
 # echo "等待60秒"
 # sleep 60
