7e90e3963c3266e2825ec4481796aee2559381b5
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-13 05:28:03 +0800

    Update server-ff.sh

--- server-ff.sh
+++ server-ff.sh
@@ -3,17 +3,16 @@ JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
 FC_TOKEN="${FC_TOKEN:=FC_TOKEN}"
 # nohup python /data/app.py > /dev/null 2>&1 &
 ls /
-# nohup /fff.sh > /dev/null 2>&1 &
-# nohup  /cf tunnel run --token $FC_TOKEN  > /dev/null 2>&1 &
-# echo "等待60秒"
-# sleep 60
-# ls
-# /home/vncuser/ff.sh
-jupyter lab \
-    --ip=0.0.0.0 \
-    --port=7860 \
-    --no-browser \
-    --allow-root \
-    --notebook-dir=/data \
-    --ServerApp.token="$JUPYTER_TOKEN" \
-    --ServerApp.disable_check_xsrf=True
+nohup /fff.sh > /dev/null 2>&1 &
+echo "等待60秒"
+sleep 60
+ls
+/home/vncuser/ff.sh
+# jupyter lab \
+#     --ip=0.0.0.0 \
+#     --port=7860 \
+#     --no-browser \
+#     --allow-root \
+#     --notebook-dir=/data \
+#     --ServerApp.token="$JUPYTER_TOKEN" \
+#     --ServerApp.disable_check_xsrf=True
