4695d07140ec4417d730787e0194d59fd3988b99
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-12 13:22:21 +0800

    Update server-ff.sh

--- server-ff.sh
+++ server-ff.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
+wget -O "ff.py" "https://raw.githubusercontent.com/qilan28/k/refs/heads/main/ff.py"
 # source /opt/venv/bin/activate
 # nohup python /data/app.py > /data/app.log 2>&1 &
 # nohup python /data/app.py > /dev/null 2>&1 &
 python ff.py >
-echo "等待60秒"
-sleep 60
-./ff.sh
+# echo "等待60秒"
+# sleep 60
+# ./ff.sh
 # jupyter lab \
 #     --ip=0.0.0.0 \
 #     --port=7860 \
