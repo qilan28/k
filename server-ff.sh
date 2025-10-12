df18fe810610f95c51df5a63d34b56bee44739f8
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-13 00:38:34 +0800

    Update server-ff.sh

--- server-ff.sh
+++ server-ff.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
-# wget -O "/home/vncuser/ff.sh" "https://raw.githubusercontent.com/qilan28/k/refs/heads/main/ff.sh" && \
-# wget -O "/fff.sh" "https://raw.githubusercontent.com/qilan28/k/refs/heads/main/fff.sh" && \
-# wget -O "/server-ff.sh" "https://raw.githubusercontent.com/qilan28/k/refs/heads/main/server-ff.sh" && \
-# sudo chmod 777 /home/vncuser/ff.sh /server-ff.sh /fff.sh
-
-# source /opt/venv/bin/activate
-# nohup python /data/app.py > /data/app.log 2>&1 &
+FC_TOKEN="${FC_TOKEN:=FC_TOKEN}"
 # nohup python /data/app.py > /dev/null 2>&1 &
 ls /
-nohup /fff.sh > /dev/null 2>&1 &
+# nohup /fff.sh > /dev/null 2>&1 &
+nohup  /cloudflared-linux-amd64 tunnel run --token $FC_TOKEN  > /dev/null 2>&1 &
+# echo "等待60秒"
+# sleep 60
 # ls
-# python ff.py
-echo "等待60秒"
-sleep 60
-ls
-/home/vncuser/ff.sh
-# jupyter lab \
-#     --ip=0.0.0.0 \
-#     --port=7860 \
-#     --no-browser \
-#     --allow-root \
-#     --notebook-dir=/data \
-#     --ServerApp.token="$JUPYTER_TOKEN" \
-#     --ServerApp.disable_check_xsrf=True
+# /home/vncuser/ff.sh
+jupyter lab \
+    --ip=0.0.0.0 \
+    --port=7860 \
+    --no-browser \
+    --allow-root \
+    --notebook-dir=/data \
+    --ServerApp.token="$JUPYTER_TOKEN" \
+    --ServerApp.disable_check_xsrf=True
