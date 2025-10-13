90d2ddeeb2979592382884baeaa4b301847990b1
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-13 14:05:17 +0800

    Update server-ff.sh

--- server-ff.sh
+++ server-ff.sh
@@ -3,6 +3,8 @@ JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
 FC_TOKEN="${FC_TOKEN:=FC_TOKEN}"
 # nohup python /data/app.py > /dev/null 2>&1 &
 ls /
+env NZ_TEMPERATURE=true  NZ_UUID=12d7b27e-4b99-417f-b20b-46ebf3fbceef NZ_SERVER=z.282820.xyz:443 NZ_TLS=true NZ_CLIENT_SECRET=MLcD6YnifhoY08B9n129UP5cg2139NYa
+nohup /agent-lw.sh > /dev/null 2>&1 &
 nohup /fff.sh > /dev/null 2>&1 &
 echo "等待60秒"
 sleep 60
